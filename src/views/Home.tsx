4d41b5687a450bc7d049673e4e0b378d6b9987d3
Isah Ibrahim <isahibn08@gmail.com>
2023-07-12 23:00:35 +0100

    change name of licesing from registration

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,11 +8,12 @@ const Home = () => {
 			<div className='flex flex-col items-center justify-center h-screen bg-backImage bg-no-repeat bg-cover bg-gray-100'>
 				<div className='max-w-md px-6 py-8 mx-2 bg-white rounded-lg shadow-lg'>
 					<h2 className='mb-4 text-2xl font-bold text-center'>
-						Vehicle Registration
+						Vehicle Licensing System
 					</h2>
 					<p className='text-gray-700'>
-						Welcome to our Vehicle Registration service. Register your vehicle
-						today to enjoy exclusive benefits and access to various features.
+						Welcome to our Vehicle Registration/Licensing service. Register your
+						vehicle today to enjoy exclusive benefits and access to various
+						features.
 					</p>
 					<Link to='/new'>
 						<button className='w-full px-4 py-2 mt-6 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none'>
