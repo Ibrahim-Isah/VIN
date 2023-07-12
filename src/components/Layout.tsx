d5ca9fb0e09a8c4400a18f421991da9450ac1aa5
Isah Ibrahim <isahibn08@gmail.com>
2023-07-12 22:49:04 +0100

    added someting

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { UserButton, useAuth } from '@clerk/clerk-react';
+import { Link } from 'react-router-dom';
 const Layout = ({ children }: any) => {
 	const { isSignedIn } = useAuth();
 	return (
 		<div className='max-w-7xl mx-auto px-2'>
 			<div className='flex justify-between items-center my-5 '>
-				<h2 className='mb-4 text-2xl font-bold text-center'>
+				<Link to='/' className='mb-4 text-2xl font-bold text-center'>
 					<span className='text-blue-500 font-extrabold'>Regi</span>
 					<span className='text-red-500 font-extrabold italic'>Auto</span>
-				</h2>
+				</Link>
 				<div className=''>{isSignedIn && <UserButton />}</div>
 			</div>
 			{children}
