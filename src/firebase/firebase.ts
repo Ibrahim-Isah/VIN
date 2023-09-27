07cf3636b10fcc35a3faedbd8df676bc336a8655
Isah Ibrahim <isahibn08@gmail.com>
2023-09-27 16:14:11 +0100

    completed

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,14 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: 'AIzaSyB6IrHFX5x-FDKHajc1GbbhaZz-c6C1kJ4',
-	authDomain: 'vehicle-reg-3b499.firebaseapp.com',
-	projectId: 'vehicle-reg-3b499',
-	storageBucket: 'vehicle-reg-3b499.appspot.com',
-	messagingSenderId: '230185200005',
-	appId: '1:230185200005:web:e5aa474355739fa6293cec',
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,28 +1,9 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
-import { notifySuccess } from '../utils/notification';
 
 const Home = () => {
 	return (
 		<Layout>
-			{/* <div className='flex flex-col items-center justify-center h-screen bg-backImage bg-no-repeat bg-cover bg-gray-100'>
-				<div className='max-w-md px-6 py-8 mx-2 bg-white rounded-lg shadow-lg'>
-					<h2 className='mb-4 text-2xl font-bold text-center'>
-						Vehicle Licensing System
-					</h2>
-					<p className='text-gray-700'>
-						Welcome to our Vehicle Registration/Licensing service. Register your
-						vehicle today to enjoy exclusive benefits and access to various
-						features.
-					</p>
-					<Link to='/new'>
-						<button className='w-full px-4 py-2 mt-6 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none'>
-							Register Vehicle
-						</button>
-					</Link>
-				</div>
-			</div> */}
 			<div className='flex flex-col items-center justify-center   bg-gray-100'>
 				<div className='min-h-screen flex justify-center items-center bg-backImage bg-no-repeat w-screen bg-cover'>
 					<div className='max-w-3xl px-6 py-8 mx-2 bg-white rounded-lg shadow-lg'>
